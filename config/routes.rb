dde4d3d47bedc190daca995586658a14011a1133
Elie Azzi <elie20azzi@gmail.com>
2026-09-05 14:36:49 +0300

    Add event REST API endpoints and JSON serialization

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,19 @@ Rails.application.routes.draw do
   get "home/index"
 
   resource :session
+
   resources :passwords, param: :token
+
   resource :registration, only: %i[new create]
 
   resources :events do
     resources :orders, only: %i[create]
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :events, only: %i[index show]
+    end
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
