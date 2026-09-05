edf9df091f48e89f9c9da27af1de78013346b822
Elie Azzi <elie20azzi@gmail.com>
2026-09-05 14:40:48 +0300

    Fix OrdersController class definition

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,16 @@
-def create
-  event = Event.find(params[:event_id])
+class OrdersController < ApplicationController
+  def create
+    event = Event.find(params[:event_id])
 
-  order = Orders::CreateService.new(
-    user: Current.user,
-    event: event
-  ).call
+    order = Orders::CreateService.new(
+      user: Current.user,
+      event: event
+    ).call
 
-  if order.persisted?
-    redirect_to event, notice: "Event booked successfully."
-  else
-    redirect_to event, alert: "Could not book this event."
+    if order.persisted?
+      redirect_to event, notice: "Event booked successfully."
+    else
+      redirect_to event, alert: "Could not book this event."
+    end
   end
 end
